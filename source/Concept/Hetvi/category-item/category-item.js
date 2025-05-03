6adf686228b22d2e7924cdc31dee9d372c4dfb08
hg1189715 <hg1189715@swccd.edu>
2025-05-03 15:58:02 -0700

    Made the necessary changes as mentioned. Modified all files.

--- source/Concept/Hetvi/category-item/category-item.js
+++ source/Concept/Hetvi/category-item/category-item.js
@@ -3,9 +3,20 @@ class CategoryItem extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({mode: "open"});
-        const template = document.getElementById("category-item-template");
-        const templateContent = template.content.cloneNode(true);
-        shadow.appendChild(templateContent);
+    }
+    async connectedCallback() {
+        // loading HTML and CSS
+        const[html, css] = await Promise.all([
+            fetch('./category-item/category-item.html').then(res => res.text()),
+            fetch('./category-item/category-item.css').then(res => res.text())
+        ]);
+
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <style>${css}</style>
+        ${html}
+        `;
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 }
 customElements.define("category-item", CategoryItem);

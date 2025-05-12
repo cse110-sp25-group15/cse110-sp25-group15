dd64dd517252471ce717e566a3e0dcbd928645aa
haoyan wan <haoyanwan@haoyans-MacBook-Air.local>
2025-05-11 17:51:53 -0700

    added vittest testing and vite for javascript bundling

--- source/components/category-item/category-item.js
+++ source/components/category-item/category-item.js
@@ -1,21 +1,17 @@
+import templateHTML from './category-item.html?raw';
+import templateCSS from './category-item.css?raw';
+
 class CategoryItem extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' }); 
+    this.attachShadow({ mode: 'open' });
   }
-  async connectedCallback() {
-    // loading HTML and CSS
-    const [html, css] = await Promise.all([
-      fetch('components/category-item/category-item.html').then((res) => res.text()),
-      fetch('components/category-item/category-item.css').then((res) => res.text()),
-    ]);
 
+  connectedCallback() {
     const template = document.createElement('template');
-    template.innerHTML = `
-        <style>${css}</style>
-        ${html}
-        `;
+    template.innerHTML = `<style>${templateCSS}</style>${templateHTML}`;
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
+
 customElements.define('category-item', CategoryItem);

982bd2e90ec9aaf343d0e9498b50e3a69b06e315
Haoyan Wan <haoyanlgd@gmail.com>
2025-05-25 21:08:14 -0700

    added public directory for static files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/cse110-sp25-group15/', 
   root: 'src',  
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        listings: 'src/list.html',
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
+      },
+    },
   },
 });
